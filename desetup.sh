d498f8a983346870b065d1a8cbec50a5fcabbe61
vps2me <dfgdfgfh@yopmail.com>
2016-07-21 20:14:06 +0800

    Update desetup.sh

--- desetup.sh
+++ desetup.sh
@@ -3,9 +3,9 @@
 apt-get update
 apt-get upgrade -y
 apt-get install python python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako -y
-wget http://download.deluge-torrent.org/source/deluge-1.3.12.tar.gz
-tar -zxvf deluge-1.3.12.tar.gz
-cd deluge-1.3.12
+wget http://download.deluge-torrent.org/source/deluge-1.3.13.tar.gz
+tar -zxvf deluge-1.3.13.tar.gz
+cd deluge-1.3.13
 python setup.py install --install-layout=deb
 cd ~
 deluged
